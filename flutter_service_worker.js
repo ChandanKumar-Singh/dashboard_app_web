e3ed3b701c70d816566269dab3271fb9eb7d9c0f
manoj19899 <manojsinghx19899@gmail.com>
2023-08-30 14:10:43 +0530

    new

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -68,8 +68,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b07086ed7824212011dcfff95eb073c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cd912ce785438f5344f92f2b8cd7e78f",
-"/": "cd912ce785438f5344f92f2b8cd7e78f",
+"index.html": "e85371d3cddb5ab80ea128f37f5e3aae",
+"/": "e85371d3cddb5ab80ea128f37f5e3aae",
 "main.dart.js": "3625f4625447649bea492e6e8b341124",
 "manifest.json": "0297e74e35082be3ba3644b30be2cbbe",
 "version.json": "b4df4eba8e9ecb9acbae547803bc9f7b"};
